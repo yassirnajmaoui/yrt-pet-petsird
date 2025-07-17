a85bfd37d3164cc0fda2827a1de7b0af3ef115c4
Yassir Najmaoui <yssrnjm@gmail.com>
2025-07-16 20:24:39 -0400

    Rename executables and adjust config

--- src/petsird_yrtpet_reconstruct.cpp
+++ src/petsird_yrtpet_reconstruct.cpp
@@ -1,9 +1,10 @@
-#include "petsird_helpers/create.h"
 
+#include "datastruct/scanner/Scanner.hpp"
 #include <iostream>
 
 int main()
 {
+    //Scanner s;
     std::cout << "It works bro" << std::endl;
     return 0;
 }
